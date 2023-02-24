00205871bbf1548911ba731508a16845949e74ec
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-02-24 05:15:49 -0800

    [Post Model] Added db Post model level validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, :summary, :content, :slug, presence: true
+
+    STATUSES = {
+      drafted:    0,
+      published: 1,
+      archived:  2
+    }.freeze
+
+    enum status: STATUSES
 end
