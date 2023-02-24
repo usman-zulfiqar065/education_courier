4aec7090b2bbd1daf1c7090833e69b05430b11dd
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-02-24 02:41:31 -0800

    [Post Model] Added db level constraints and run migration

--- db/migrate/20230224103015_create_posts.rb
+++ db/migrate/20230224103015_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :summary
-      t.text :content
-      t.integer :status
-      t.string :slug
+      t.string :title, null: false
+      t.text :summary, null: false
+      t.text :content, null: false
+      t.integer :status, null: false
+      t.string :slug, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_103015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "summary", null: false
+    t.text "content", null: false
+    t.integer "status", null: false
+    t.string "slug", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
