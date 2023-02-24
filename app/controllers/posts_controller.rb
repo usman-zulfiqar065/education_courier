ecf71338b45d010f6ff0b636b8f874a516ea7b54
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-02-24 08:18:25 -0800

    [Post Model] Added Show action and related view/partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: :show
+
   def index
     @posts = Post.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
   end
@@ -19,5 +20,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def get_post
+    @post = Post.find(params[:id])
   end
 end
