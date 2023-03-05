93bd9367a247ccf371a3e662823db759a1c2f8b0
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-03-05 09:17:13 -0800

    [Pages Module] Added a pages controller for different pages like Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  root to: 'pages#home'
 
   resources :posts do 
-    resources :comments, shallow: true
+    resources :comments, shallow: true, except: %i[ show new edit ]
   end
   resources :users
 end
