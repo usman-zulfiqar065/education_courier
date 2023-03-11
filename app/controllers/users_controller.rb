0539f84f2f003b6a68d861ac348940127f469532
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-03-11 09:43:00 -0800

    Added flash divs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.find_or_initialize_by(email: params[:email])
     if @user.save
-      redirect_to root_path, notice:  'User was successfully created.'
+      redirect_to root_path, notice:  'Thankx! for your subscription'
     else
       redirect_to root_path, status: :unprocessable_entity
     end
