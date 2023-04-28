b92618df42e13dff591b6e26f1eda0bfb97a507a
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-04-28 07:59:17 -0700

    Added rubocop for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
-  before_action :get_category, only: %i[ edit destroy ]
+  before_action :set_category, only: %i[edit destroy]
 
   def index
     @categories = Category.all
@@ -14,58 +16,71 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if @category.save
         flash.now[:notice] = 'Category Created Successfully'
-        format.turbo_stream { render turbo_stream: [
-          turbo_stream.replace('new_category_form', partial: 'new_category_btn'),
-          turbo_stream.prepend('body_tag', partial: 'shared/toast'),
-          turbo_stream.append('categories', partial: @category)
-        ]}
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace('new_category_form', partial: 'new_category_btn'),
+            turbo_stream.prepend('body_tag', partial: 'shared/toast'),
+            turbo_stream.append('categories', partial: @category)
+          ]
+        end
       else
         flash.now[:error] = 'Unable to create category'
-        format.turbo_stream { render turbo_stream:  [
-          turbo_stream.prepend('new_category_form', partial: 'shared/error_messages', locals: { object: @category }),
-          turbo_stream.prepend('body_tag', partial: "shared/toast")
-        ]}
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.prepend('new_category_form', partial: 'shared/error_messages', locals: { object: @category }),
+            turbo_stream.prepend('body_tag', partial: 'shared/toast')
+          ]
+        end
       end
     end
   end
 
   def edit; end
 
-  def update 
+  def update
     @category = Category.find(params[:id])
-    if @category.update(category_params)
-      redirect_to categories_path, notice: 'Category Updated Successfully'
-    else
-      respond_to do |format|
-        flash.now[:error] = 'Unable to update category'
-        format.turbo_stream { render turbo_stream:  [
-          turbo_stream.prepend("#{helpers.dom_id(@category)}", partial: 'shared/error_messages', locals: { object: @category }),
-          turbo_stream.prepend('body_tag', partial: "shared/toast")
-        ]}
+    respond_to do |format|
+      if @category.update(category_params)
+        flash.now[:notice] = 'Category Updated Successfully'
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace("category_#{@category.id}_cart", partial: @category),
+            turbo_stream.prepend('body_tag', partial: 'shared/toast')
+          ]
+        end
+      else
+        format.turbo_stream do
+          flash.now[:error] = 'Unable to update category'
+          render turbo_stream: [
+            turbo_stream.prepend(helpers.dom_id(@category).to_s, partial: 'shared/error_messages',
+                                                                 locals: { object: @category }),
+            turbo_stream.prepend('body_tag', partial: 'shared/toast')
+          ]
+        end
       end
     end
   end
 
   def destroy
-    begin
-      @category.destroy
-      respond_to do |format|
-        flash.now[:notice] = 'Category deleted successfully'
-        format.turbo_stream { render turbo_stream: 
-        [ turbo_stream.prepend('body_tag', partial: "shared/toast"),
-        turbo_stream.remove(@category)
-      ]}
+    @category.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'Category deleted successfully'
+      format.turbo_stream do
+        render turbo_stream:
+      [turbo_stream.prepend('body_tag', partial: 'shared/toast'),
+       turbo_stream.remove("category_#{@category.id}_cart")]
       end
-    rescue StandardError => e
-      flash.now[:error] = 'Unable to delete category'
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend('body_tag', partial: "shared/toast") }
-      end
+    end
+  rescue StandardError
+    flash.now[:error] = 'Unable to delete category'
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.prepend('body_tag', partial: 'shared/toast') }
     end
   end
 
-  private 
-  def get_category
+  private
+
+  def set_category
     @category = Category.find(params[:id])
   end
 
