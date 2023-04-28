ecb580498ca81aa84127cb041752e9bfb72ada34
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-04-28 16:44:40 -0700

    Added model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ApplicationRecord
 
   enum status: STATUSES
 
+  belongs_to :user
   belongs_to :category
   has_many :comments, dependent: :destroy
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,5 @@ class User < ApplicationRecord
   enum role: ROLES
 
   has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
