83a9481898f7addd71fd665414f901af375d7ddd
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-04-28 18:47:47 -0700

    Added sign_in and sign_out btns

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
   def home
     @posts = Post.published.general
     @featured_posts = Post.published.featured
