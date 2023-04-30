0c9602d06e931c897a04cbd2e0cdd4682a1baa73
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-04-30 07:13:47 -0700

    Updated redirect route for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_to posts_path, notice: 'Post created successfully'
+      redirect_to user_posts_path, notice: 'Post created successfully'
     else
       render :new, status: :unprocessable_entity
     end
