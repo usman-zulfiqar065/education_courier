c29b5327f73ec8f4f45cf16c467691fd0cb532fc
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-01 13:24:31 -0700

    Updated permitted params for devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :error
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :role) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :role)
+    end
+  end
 end
