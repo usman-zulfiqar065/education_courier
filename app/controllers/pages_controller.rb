cb4d669b8bda88326547193724f47c015c42a920
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-01 18:24:02 -0700

    Home page post view customization

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @posts = Post.published.general
-    @featured_posts = Post.published.featured.last(6)
+    @posts = general_posts
+    @featured_posts = featured_posts.last(6)
+    @categories = Category.all.select(:id, :name)
+  end
+
+  private 
+  def featured_posts
+    Post.published.featured.select(:id, :title, :user_id, :published_at, :read_time, :category_id)
+        .includes(:user, :category)
+  end
+
+  def general_posts
+    Post.published.general.select(:id, :title, :user_id, :published_at, :read_time, :summary, :category_id)
+        .includes(:user, :category)
   end
 end
