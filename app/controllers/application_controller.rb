b19f2edbd8a8d331df720e4c9f62e4c3f899629e
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-08 17:39:26 -0700

    Fixed comments creation bug for guest user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, :unless => :devise_controller?
   add_flash_types :error
 
   protected
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password, :current_password, :role)
     end
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url) if params[:controller] != 'comments'
   end
 end
