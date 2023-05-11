c03a2be832e41b5e0f0a39dafc74242a410c80e1
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-11 12:06:20 -0700

    Added show action for current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users 
+  devise_for :users
 
-  resources :users, only: [:create], shallow: true do
+  resources :users, only: %i[create show], shallow: true do
     resources :blogs do
       resources :comments, except: %i[show]
     end
