bf5eb8f9d6e45767cdf04d80c44fa6943da41b73
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-23 10:23:33 -0700

    Some UI Fixes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -12,7 +12,6 @@ module LikesHelper
   def add_like_btn(likeable_id, likeable_type)
     like = Like.find_by(likeable_id:, likeable_type:)
     like_action_path = likes_path(like: { likeable_id:, likeable_type: })
-    style = likeable_type == 'Comment' ? 'pt-0 px-0' : 'p-0'
     if user_signed_in? && current_user.liked(likeable_id, likeable_type)
       method = 'delete'
       like_action_path = like_path(like, like: { likeable_id:, likeable_type: })
@@ -20,10 +19,10 @@ module LikesHelper
 
     if user_signed_in?
       render inline: "<%= button_to add_like_icon('#{likeable_id}', '#{likeable_type}').html_safe,
-                          '#{like_action_path}', method: '#{method || 'post'}', class: 'text-dark me-2 btn #{style}',
+                          '#{like_action_path}', method: '#{method || 'post'}', class: 'text-dark me-2 align-self-center btn p-0',
                           'data-turbo-frame': '_top' %>"
     else
-      render inline: "<button class='text-dark me-2 btn #{style}' data-bs-toggle='modal'
+      render inline: "<button class='text-dark me-2 btn p-0 align-self-center' data-bs-toggle='modal'
                               data-bs-target='#exampleModal'>#{add_like_icon(likeable_id, likeable_type)}</button>"
     end
   end
@@ -33,7 +32,7 @@ module LikesHelper
     if user_signed_in?
       render inline: "<%= link_to '#{btn_text}'.html_safe, '#{new_blog_comment_path(comment.blog, parent_id: comment)}',
                         'data-turbo-frame': '#{dom_id(comment)}_child_comment',
-                        class: 'ps-2 text-dark me-2 py-0 btn' %>"
+                        class: 'ps-2 text-dark me-2 py-0 btn align-self-center' %>"
     else
       render inline: "<button class='ps-2 text-dark me-2 py-0 btn' data-bs-toggle='modal'
                               data-bs-target='#exampleModal'>#{btn_text}</button>"
