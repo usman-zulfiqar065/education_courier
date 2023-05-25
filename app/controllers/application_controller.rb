5ca5d3b657ea7961ee4bfdbb78641b2c86643562
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-24 18:17:30 -0700

    Refactored users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_current_location, unless: :devise_controller?
   add_flash_types :error
 
@@ -7,6 +8,11 @@ class ApplicationController < ActionController::Base
 
   def store_current_location
     store_location_for(:user, request.url) if params[:controller] != 'comments'
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password role])
   end
 
   def authenticate_admin_user!
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,9 +2,6 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    before_action :configure_sign_up_params, only: [:create]
-    before_action :configure_account_update_params, only: [:update]
-
     def new
       @user = User.new
       subscriber_user if params[:email].present? && params[:role] == 'subscriber'
@@ -13,25 +10,25 @@ module Users
 
     protected
 
-    def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password role])
-    end
-
-    def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password role])
-    end
-
     def subscriber_user
       user = User.find_by(email: params[:email])
       if user.present?
-        flash[:notice] = 'Thankx for your subscription'
-        user.update(role: 'subscriber') if user.member?
-        redirect_to root_path
+        handle_subscriber_creation_success(user)
       else
-        flash.now[:alert] = 'You need to sign up before continuing.'
-        @user.email = params[:email]
-        @user.role = params[:role]
+        handle_failed_subscriber_creation
       end
+    end
+
+    def handle_subscriber_creation_success(user)
+      flash[:notice] = 'Thankx for your subscription'
+      user.update(role: 'subscriber') if user.member?
+      redirect_to root_path
+    end
+
+    def handle_failed_subscriber_creation
+      flash.now[:alert] = 'You need to sign up before continuing.'
+      @user.email = params[:email]
+      @user.role = params[:role]
     end
   end
 end
