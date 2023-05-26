3ad9a25502fcd515edc38a019dcc9b7e830e434d
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-05-25 17:13:01 -0700

    Refactored start writing btn

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,13 @@ module BlogsHelper
   def video_embed_link(original_link)
     original_link.sub('watch?v=', 'embed/')
   end
+
+  def add_start_writing_btn
+    if user_signed_in?
+      render inline: "<%= link_to 'Start Writing', user_blogs_path(current_user), class: 'btn btn-info' %>"
+    else
+      render inline: "<button class='btn btn-info' data-bs-toggle='modal'
+                              data-bs-target='#exampleModal'>Start Writing</button>"
+    end
+  end
 end
