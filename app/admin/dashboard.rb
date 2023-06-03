2a072f084e6e4acc436add103dbaa887e997f244
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-06-02 20:43:36 -0700

    Customized admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -42,33 +42,31 @@ end
 
 like_user_block = proc do
   column :user do |like|
-    current_user.admin? && (link_to like.user.name admin_user_path(like.user)) || like.user.name
+    current_user.admin? && (link_to like.user.name, admin_user_path(like.user)) || like.user.name
   end
 end
 
 states_pannel_block = proc do
-  panel "Today's States" do
-    para "Total Blogs: #{current_user.admin_dashboard_blogs.created_today.count}"
-    para "Total Comments: #{current_user.blog_comments.created_today.count}"
-    para "Total Blog likes: #{Like.dashboard_status(current_user, 'Blog').created_today.count}"
-    para "Total Comment likes: #{Like.dashboard_status(current_user, 'Comment').created_today.count}"
+  sidebar "Today's Status" do
+    ul do
+      li "Total Blogs:         #{current_user.admin_dashboard_blogs.created_today.count}"
+      li "Total Comments:      #{current_user.blog_comments.created_today.count}"
+      li "Total Blog likes:    #{Like.dashboard_status(current_user, 'Blog').created_today.count}"
+      li "Total Comment likes: #{Like.dashboard_status(current_user, 'Comment').created_today.count}"
+    end
   end
 end
 
 blogs_pannel_block = proc do
-  columns do
-    column do
-      panel "Today's Blogs" do
-        table_for current_user.admin_dashboard_blogs.created_today do
-          column :id
-          column('title') { |blog| link_to blog.title, admin_blog_path(blog) }
-          column :status if current_user.admin?
-          column :user
-          column :created_at
-          column('Likes Count') { |blog| blog.likes.count }
-          column('Comments Count') { |blog| blog.comments.count }
-        end
-      end
+  panel "Today's Blogs" do
+    table_for current_user.admin_dashboard_blogs.created_today do
+      column :id
+      column('title') { |blog| link_to blog.title, admin_blog_path(blog) }
+      column :status if current_user.admin?
+      column :user
+      column :created_at
+      column('Likes Count') { |blog| blog.likes.count }
+      column('Comments Count') { |blog| blog.comments.count }
     end
   end
 end
@@ -101,9 +99,17 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    instance_eval(&states_pannel_block)
-    instance_eval(&blogs_pannel_block)
-    instance_eval(&comments_pannel_block)
-    instance_eval(&likes_pannel_block)
+    tabs do
+      tab :todays_blogs do
+        instance_eval(&blogs_pannel_block)
+      end
+      tab :todays_comments do
+        instance_eval(&comments_pannel_block)
+      end
+      tab :todays_likes do
+        instance_eval(&likes_pannel_block)
+      end
+    end
   end
+  instance_eval(&states_pannel_block)
 end
