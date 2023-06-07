83df905aaf6de1bb1bef48ea0496da4bd4a8b2ec
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-06-07 08:42:52 -0700

    Updated response on subscribe action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,10 @@ class UsersController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present?
       user.update(is_subscriber: true)
-      redirect_to root_path, notice: 'Thankx for your subscription'
+      respond_to do |format|
+        flash.now[:notice] = 'Thankx for your subscription'
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend('body_tag', partial: 'shared/toast') }
+      end
     else
       redirect_to new_user_registration_path, alert: 'You need to sign up before continuing.'
     end
