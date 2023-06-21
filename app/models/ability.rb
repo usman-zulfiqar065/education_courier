48b3b0e9f33b03881f9aa638eee855e9c22d84ee
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-06-21 11:29:50 -0700

    Updated ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,12 +7,14 @@ class Ability
     can :read, Blog, Blog.published, &:published?
     can :read, Comment
     can :read, Category
+    can :read, Like
     if user&.admin?
       can :manage, :all
     elsif user.present?
       can :manage, Blog, user_id: user.id
-      can :manage, Comment, user_id: user.id
+      can :read, user.blog_comments
       can :manage, User, id: user.id
+      can :read, User
       can :read, ActiveAdmin::Page, name: 'Dashboard', namespace_name: 'admin'
     end
   end
