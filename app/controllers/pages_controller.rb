1a5d09757a0b5da7a826a367ba57e9aa2dcbf88f
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-06-23 17:54:30 -0700

    Fixed UI for paginator div

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @blogs = general_blogs.in_ascending_order
+    @pagy, @blogs = pagy(general_blogs.in_ascending_order)
     @featured_blogs = featured_blogs.in_ascending_order.last(6)
     @categories = Category.all.select(:id, :name)
   end
